24fa555fd778bc2563601e1fe2aa064568ff304d
Vivek M Nair <vivekkevivo@gmail.com>
2021-11-04 14:31:45 -0700

    date utc change

--- packages/health/lib/src/health_factory.dart
+++ packages/health/lib/src/health_factory.dart
@@ -175,10 +175,15 @@ class HealthFactory {
 
           //Convert dateTime to UTC
           DateTime from = DateTime.fromMillisecondsSinceEpoch(
-              element['date_from'],
-              isUtc: true);
-          DateTime to = DateTime.fromMillisecondsSinceEpoch(element['date_to'],
-              isUtc: true);
+            element['date_from'],
+          );
+          DateTime to = DateTime.fromMillisecondsSinceEpoch(
+            element['date_to'],
+          );
+
+          from = DateTime.utc(
+              from.year, from.month, from.day, from.hour, from.minute);
+          to = DateTime.utc(to.year, to.month, to.day, to.hour, to.minute);
 
           // Add 2 hours to the selected dataTypes
           // if (hourErrorhealthTypes.contains(dataType)) {
